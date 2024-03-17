97f11d19b853d509b500bf373996d9ca777b6d32
KnightNimbus <ty.s.spain@gmail.com>
2024-03-17 23:34:53 +0000

    added variables for search bar and search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
-const APIkey = "8a80624085152b2b83175925bacc2328";
+const searchBar = document.getElementById("searchBar");
+const searchButton = document.getElementById("searchButton")
 
 let cityName = [];
 
+const APIkey = "8a80624085152b2b83175925bacc2328";
 const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}`;
