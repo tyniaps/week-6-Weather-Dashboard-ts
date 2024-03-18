101f0a2142f0c663f279df8374c5d7dd1a9c6f42
KnightNimbus <ty.s.spain@gmail.com>
2024-03-18 00:13:24 +0000

    added event listener to search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
-const searchBar = document.getElementById("searchBar");
-const searchButton = document.getElementById("searchButton")
+const searchBar = document.getElementById("search-bar");
+const searchButton = document.getElementById("search-button")
 
-let cityName = [];
+   
 
 const APIkey = "8a80624085152b2b83175925bacc2328";
-const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}`;
+// const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}`;
+
+const cityDetails = () => {
+   const cityName = searchBar.value.trim();
+   if(!cityName)
+   return;
+}
+
+searchButton.addEventListener('click', cityDetails);
