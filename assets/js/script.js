d090766a4458bd7ec7deff5d504bfcd8d9a5afc0
KnightNimbus <ty.s.spain@gmail.com>
2024-03-20 15:30:16 +0000

    removed certain code (clean slate)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,4 @@
-const searchBar = document.getElementById("search-bar");
 const searchButton = document.getElementById("search-button")
-
-   
-
+const cityInput = document.getElementById("search-bar").value
 const APIkey = "8a80624085152b2b83175925bacc2328";
-// const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}`;
 
-const cityDetails = () => {
-   const cityName = searchBar.value.trim();
-   if(!cityName)
-   return;
-}
-
-searchButton.addEventListener('click', cityDetails);
