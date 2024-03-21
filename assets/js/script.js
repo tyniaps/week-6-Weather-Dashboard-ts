b0b8b27563b6ab31638e854332406293ca53a31f
KnightNimbus <ty.s.spain@gmail.com>
2024-03-21 03:03:34 +0000

    opened function for handling fetch request. user to be prompted to enter city name if search button is clicked with no search bar text input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 const searchButton = document.getElementById("search-button");
 
-
+function getWeatherFromAPI() {
    const cityInput = document.getElementById("search-bar").value;
    const apiKey = "777c5cbaff1a8d260ade05ddf516e40f";
 
-
-
+    if (!cityInput) {
+    alert("Please enter a city");
+    return;
+  }
+}
