683e47d8b02fef2da28567ab9aa1cfa98bf438a9
KnightNimbus <ty.s.spain@gmail.com>
2024-03-21 03:08:35 +0000

    added fetch call to API url - any errors to show in console and alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,16 @@ function getWeatherFromAPI() {
 
   const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`;
 
+    fetch(queryURL)
+    .then(response => response.json())
+    .then(data => {
+     displayCurrentForecast(data);
+    })
+    .catch(error => {
+    console.error("Error fetching weather forecast data:", error);
+    alert("Error fetching weather forecast data, please try again");
+    });
+
 }
+
+
